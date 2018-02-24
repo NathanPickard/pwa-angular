e30da0228c5a8ea70e0e846532f327ed5a0bae77
Nathan Pickard <nathan.pickard87@gmail.com>
2018-02-23 22:37:20 -0800

    pre-caching app shell with ngsw

--- coffeeapp/src/app/app.module.ts
+++ coffeeapp/src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { GeolocationService } from './geolocation.service';
@@ -8,7 +10,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
   MatButtonModule, MatIconModule, MatInputModule, MatSelectModule, MatSliderModule,
-  MatToolbarModule, MatCardModule, MatSlideToggleModule
+  MatToolbarModule, MatCardModule, MatSlideToggleModule, MatSnackBarModule
 } from '@angular/material';
 import 'hammerjs';
 import { ListComponent } from './list/list.component';
@@ -33,10 +35,11 @@ const routes: Routes = [
   ],
   imports: [
     RouterModule.forRoot(routes),
+    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
     FormsModule, HttpModule,
     BrowserModule, BrowserAnimationsModule, MatButtonModule,
     MatIconModule, MatInputModule, MatSelectModule, MatSliderModule,
-    MatToolbarModule, MatCardModule, MatSlideToggleModule
+    MatToolbarModule, MatCardModule, MatSlideToggleModule, MatSnackBarModule
   ],
   providers: [GeolocationService, DataService],
   bootstrap: [AppComponent]
